7076261be67f3ad6665d63f69015c2780d58f6fe
diaz farindra <farindrad@gmail.com>
2021-09-23 18:52:40 +0700

    bulk update

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return User::with('tasks')->get();
+    }
+}
